63af287937bb94ae7c2816d4e6c9fd9cf630bd79
osm3000 <omar.samir3000@gmail.com>
2014-06-22 23:20:54 +0200

    Committing the modified code + the modified tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -10,17 +10,17 @@
         # Print the output
 
 # Read both the training and the test data sets
-dataTraining_0 <- read.table("./X_train.txt")
-dataTest_0 <- read.table("./X_test.txt")
+dataTraining_0 <- read.table("../X_train.txt")
+dataTest_0 <- read.table("../X_test.txt")
 
 dataTraining <- dataTraining_0
 dataTest <- dataTest_0
 
 #Loading labels for both data sets
-sub_test_Train <- read.table("./subject_train.txt")
-sub_test_Test <- read.table("./subject_test.txt")
-y_test0 <- read.table("./y_test.txt")
-y_train0 <- read.table("./y_train.txt")
+sub_test_Train <- read.table("../train/subject_train.txt")
+sub_test_Test <- read.table("../test/subject_test.txt")
+y_test0 <- read.table("../test/y_test.txt")
+y_train0 <- read.table("../train/y_train.txt")
 y_test <- y_test0
 y_train <- y_train0
 
@@ -40,7 +40,7 @@ y_train[y_train == 1] <- "WALKING"
 
 
 # Read the names of variables
-dataLabels <- read.table("./features.txt")
+dataLabels <- read.table("../features.txt")
 features_names <- (dataLabels[,2])
 features_names <- as.character(features_names)
 features_names <- c("Subject","Activity Code","Activity",features_names)
@@ -60,9 +60,13 @@ mergedData <- rbind(dataTraining,dataTest)
 toMatch <- c("Subject","Activity Code","Activity",".*mean\\(\\).*-X$",".*mean\\(\\).*-Y$",".*mean\\(\\).*-Z$", ".*std\\(\\).*-X$",".*std\\(\\).*-Y$",".*std\\(\\).*-Z$")
 matches <- unique (grep(paste(toMatch,collapse="|"), features_names, value=TRUE))
 mergedData2 <- mergedData[,matches] #Getting only the needed measurements columns
-
+#write.csv(mergedData2, file = "koo.csv")
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-mergedData3 <- list()
+#mergedData3 <- list()
+#mergedData3 <- data.frame()
+mergedData3 <- NULL
+mergedData3 <- rbind(colnames(mergedData))
+#colnames(mergedData3) <- c("3","4")
 for (subject in 1:30){
         for (activity in 1:6){
                 filtered_list <- mergedData[mergedData$"Activity Code" == activity & mergedData$Subject == subject,4:564]
@@ -86,11 +90,15 @@ for (subject in 1:30){
                         temp <- "LAYING"
                 }
                 list_means <- c("Subject" = subject,"Activity Code" = activity,"Activity" = temp,list_means)
+                list_means <- as.character(list_means)
                 mergedData3 <- rbind(mergedData3,list_means)
         }
 }
 row.names(mergedData3) <- NULL
 mergedData3 <- as.data.frame(mergedData3)
+colnames(mergedData3) <-names(mergedData)
+mergedData3 <- mergedData3[-1,]
 
 # Print the output
-capture.output( print(mergedData3, print.gap=3), file="tidy_data.txt")
+#capture.output( print(mergedData3, print.gap=3), file="tidy_data.txt")
+write.csv(mergedData3, file = "Tidy_data.csv")
